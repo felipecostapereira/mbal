5523fde9714768c4fc67efe58372a77c82fce683
felipcostapereira <felipecostapereira@gmail.com>
2024-03-11 13:04:47 -0300

    day2

--- mbal.py
+++ mbal.py
@@ -7,6 +7,7 @@ import seaborn as sns
 help_strings = {
     'sliders': f'(min,max) = ($\mu-3\sigma,\mu+3\sigma$)',
     'rf': f'Initial guess on the recovery factor, to populate number of wells in experiments',
+    'norm': f'Values will be always noramlized',
 }
 
 list_mecanismos = {
@@ -17,9 +18,8 @@ list_mecanismos = {
 unitsOil = st.sidebar.radio('Oil Units:',['MMm³','MMBBL'], horizontal=True)
 unitsGas = st.sidebar.radio('Gas Units:',['MMm³','TCF'], horizontal=True)
 
-tabRes, tabFluid, tabVFP, tabSampling, tabMBAL, tabMatBal,  tabResults =  st.tabs(['Reservoir', 'Fluid', 'VFP', 'Sampling', 'MBAL', 'MatBal Spreadsheet', 'Results'])
+tabRes, tabFluid, tabVFP, tabSampling, tabMBAL, tabMatBal,  tabResults =  st.tabs(['Reservoir:mount_fuji:', 'Fluid:oil_drum:', 'VFP:chart_with_upwards_trend:', 'Sampling:game_die:', 'MBAL	:m:', 'Spreadsheet:chart_with_downwards_trend:', 'Results:signal_strength:'])
 with tabRes:
-
     col1, col2 = st.columns(2)
     with col1:
         resType = st.radio('Reservoir type:',['Oil','Gas'], horizontal=True)
@@ -29,7 +29,7 @@ with tabRes:
         else:
             vgip = st.slider('VGIP (MMm³)', 0, 5000, (1000,2000), help=help_strings['sliders'])
             voip = 0
-        rf = st.number_input('Target RF (%)', 10, 50, 20, help=help_strings['rf'])
+        rf = st.number_input('Target RF (%)', 1, 100, 20, help=help_strings['rf'])
         selected_mecanisms = st.multiselect(f'Production Mechanism - {resType}', list_mecanismos[resType])
     with col2:
         if resType == 'Oil':
@@ -58,11 +58,53 @@ with tabRes:
         j.figure.axes[0].set_ylabel('Np/well')
         j.figure.axes[0].set_xlim((0, 200))
         j.figure.axes[0].set_ylim((0, 100))
-        st.pyplot(j.figure)
+        st.pyplot(j.figure, clear_figure=True)
 
+with tabFluid:
+    col1, col2 = st.columns(2)
+    with col1:
+        nfluids = st.number_input('How many Fluids?',1,5,3)
+        fluids = [f+1 for f in range(nfluids)]
+        fluid_prob = [st.number_input(f"Probability/weight of fluid {i+1}", 1,100,1, help=help_strings['norm']) for i in range(nfluids)]
+        fluid_prob = fluid_prob/np.sum(fluid_prob)
+
+    with col2:
+        plotf = sns.barplot(x=fluids, y=fluid_prob, hue=fluids, palette='tab10')
+        plotf.set_xlabel('Fluid')
+        plotf.set_ylabel('Probability')
+        st.pyplot(plotf.figure)
 
 with tabSampling:
-    st.header("opções de amostragem: montecarlo, hipercubo latino, numero de rodadas MBAL etc....., mostrar um tabelao pandas com os experimentos")
+    nruns = st.number_input('MBAL runs:', 10, None, 100, step=100)
+    pi_dist = np.random.normal(pi_mean, pi_std, nruns)
+    np_well_dist = np.random.normal(np_well_mean, np_well_std, nruns)
+    fluid_dist = np.random.choice(fluids, size=nruns, p=fluid_prob)
+    if resType=='Oil':
+        vol = np.random.normal(np.mean(voip),(voip[1]-voip[0])/6,nruns)
+    else:
+        vol = np.random.normal(np.mean(vgip),(voip[1]-voip[0])/6,nruns)
+
+    samples = pd.DataFrame(
+        {
+            'Vol':vol,
+            'Np_Well':np_well_dist,
+            'Pi':pi_dist,
+            'Fluid':fluid_dist,
+        },
+        index=[i+1 for i in range(nruns)]
+    )
+
+    samples['Np'] = samples['Vol'] * rf/100
+    samples['Wells'] = samples['Np']/samples['Np_Well']
+    samples = samples.round(0)
+
+    'Experiments'
+    st.write(samples)
+    'Stats'
+    st.write(samples.describe().loc[['mean', 'std', 'min', 'max']].round(0))
+
+
+
 with tabMBAL:
     st.header("Acompanhamento das rodadas e parametrização do openserver")
 with tabMatBal:
