c8847d0ef8fe2d50da744d09fe2646ae7c743335
felipcostapereira <felipecostapereira@gmail.com>
2024-03-13 15:15:22 -0300

    title, new dists, expander

--- mbal.py
+++ mbal.py
@@ -6,9 +6,10 @@ import seaborn as sns
 import datetime
 import requests
 
+nsamples = 1000
 
 help_strings = {
-    'sliders': f'(min,max) = ($\mu-3\sigma,\mu+3\sigma$)',
+    'sliders': f'(min,max) = ($\mu-3\sigma,\mu+3\sigma$) if Normal',
     'rf': f'Initial guess on the recovery factor, to populate number of wells in experiments',
     'norm': f'Values will be always noramlized',
     'pvt': f'Please attach PVT file for this fluid',
@@ -22,6 +23,8 @@ list_mecanismos = {
 unitsOil = st.sidebar.radio('Oil Units:',['MMm³','MMBBL'], horizontal=True)
 unitsGas = st.sidebar.radio('Gas Units:',['MMm³','TCF'], horizontal=True)
 
+st.subheader('_Reservoir Potential evaluation_')
+
 tabRes, tabFluid, tabVFP, tabSampling, tabSchedule, tabMBAL, tabMatBal,  tabResults =  st.tabs([
     'Reservoir:mount_fuji:',
     'Fluid:oil_drum:',
@@ -32,46 +35,72 @@ tabRes, tabFluid, tabVFP, tabSampling, tabSchedule, tabMBAL, tabMatBal,  tabResu
     'Spreadsheet:chart_with_downwards_trend:',
     'Results:signal_strength:'
     ])
+
 with tabRes:
-    col1, col2 = st.columns(2)
-    with col1:
-        resType = st.radio('Reservoir type:',['Oil','Gas'], horizontal=True)
-        if resType == 'Oil':
-            voip = st.slider('VOIP (MMBBL)', 0, 5000, (1000,3000), help=help_strings['sliders'])
-            vgip = 0
-        else:
-            vgip = st.slider('VGIP (MMm³)', 0, 5000, (1000,2000), help=help_strings['sliders'])
-            voip = 0
-        rf = st.number_input('Target RF (%)', 1, 100, 20, help=help_strings['rf'])
-        selected_mecanisms = st.multiselect(f'Production Mechanism - {resType}', list_mecanismos[resType])
-    with col2:
-        if resType == 'Oil':
-            ploto = sns.kdeplot(np.random.normal(np.mean(voip),(voip[1]-voip[0])/6,1000), fill=True)
-            ploto.set_xlabel("VOIP")
-            st.pyplot(ploto.figure, clear_figure=True)
-        else:
-            plotg=sns.kdeplot(np.random.normal(np.mean(vgip),(vgip[1]-vgip[0])/6,1000), fill=True)
-            plotg.set_xlabel("VGIP")
-            st.pyplot(plotg.figure, clear_figure=True)
+    with st.expander('Reservoir Data'):
+        st.subheader('Reservoir Data', divider='blue')
+        col1, col2 = st.columns(2)
+        with col1:
+            resType = st.radio('Reservoir type:',['Oil','Gas'], horizontal=True)
 
+            rf = st.number_input('Target RF (%)', 1, 100, 20, help=help_strings['rf'])
+            selected_mecanisms = st.multiselect(f'Production Mechanism - {resType}', list_mecanismos[resType])
 
-    st.divider()
+        with col2:
+            voip_dist = st.selectbox('VOIP distribution',options=['Normal','Uniform','Triangular'])
+            if resType == 'Oil':
+                voip = st.slider('VOIP (MMBBL)', 0, 5000, (1000,3000), help=help_strings['sliders'])
+                vgip = (0,0)
+            else:
+                vgip = st.slider('VGIP (MMm³)', 0, 5000, (1000,2000), help=help_strings['sliders'])
+                voip = (0,0)
+            voip_mean = np.mean(voip)
+            voip_stdev = (voip[1]-voip[0])/6
+            vgip_mean = np.mean(vgip)
+            vgip_stdev = (vgip[1]-vgip[0])/6
 
-    col3, col4 = st.columns(2)
-    with col3:
-        np_well = st.slider('Np/well:', 0, 100, (30,60), help=help_strings['sliders'])
-        np_well_mean = np.mean(np_well)
-        np_well_std = (np_well[1]-np_well[0])/6
-        pi = st.slider('Productivity Index (m³/d/kgf/cm²):', 0, 200, (50,100), help=help_strings['sliders'])
-        pi_mean = np.mean(pi)
-        pi_std = (pi[1]-pi[0])/6
-    with col4:
-        j = sns.jointplot(y=np.random.normal(np_well_mean, np_well_std, 1000), x=np.random.normal(pi_mean, pi_std, 1000), kind='hex', height=4)
-        j.figure.axes[0].set_xlabel('PI')
-        j.figure.axes[0].set_ylabel('Np/well')
-        j.figure.axes[0].set_xlim((0, 200))
-        j.figure.axes[0].set_ylim((0, 100))
-        st.pyplot(j.figure, clear_figure=True)
+            if resType == 'Oil':
+                if voip_dist == 'Normal':                   ploto = sns.kdeplot(np.random.normal(voip_mean,voip_stdev,nsamples), fill=True)
+                elif voip_dist == 'Lognormal':              ploto = sns.kdeplot(np.random.normal(np.log(voip_mean+(voip_stdev**2)/2),5,nsamples), fill=True)
+                elif voip_dist == 'Triangular':             ploto = sns.kdeplot(np.random.triangular(voip[0], voip_mean, voip[1], nsamples), fill=True)
+                else:                                       ploto = sns.kdeplot(np.random.uniform(voip[0],voip[1], nsamples), fill=True)
+                ploto.set_xlabel("VOIP")
+                st.pyplot(ploto.figure, clear_figure=True)
+            else:
+                if voip_dist == 'Normal':                   plotg = sns.kdeplot(np.random.normal(vgip_mean,vgip_stdev,nsamples), fill=True)
+                elif voip_dist == 'Lognormal':              plotg = sns.kdeplot(np.random.normal(vgip_mean,vgip_stdev,nsamples), fill=True)
+                elif voip_dist == 'Triangular':             plotg = sns.kdeplot(np.random.triangular(vgip[0],vgip_mean,vgip[1],nsamples), fill=True)
+                else:                                       plotg = sns.kdeplot(np.random.uniform(vgip[0],vgip[1],nsamples), fill=True)
+                plotg.set_xlabel("VGIP")
+                st.pyplot(plotg.figure, clear_figure=True)
+
+    with st.expander('Production Metrics'):
+        st.subheader('Production Metrics', divider='blue')
+
+        col3, col4 = st.columns(2)
+        with col3:
+            metric_dist = st.selectbox('Metrics parameter distribution',options=['Normal','Uniform','Triangular'])
+            np_well = st.slider('Np/well:', 0, 100, (30,60), help=help_strings['sliders'])
+            np_well_mean = np.mean(np_well)
+            np_well_std = (np_well[1]-np_well[0])/6
+            pi = st.slider('Productivity Index (m³/d/kgf/cm²):', 0, 200, (50,100), help=help_strings['sliders'])
+            pi_mean = np.mean(pi)
+            pi_std = (pi[1]-pi[0])/6
+
+        with col4:
+            if metric_dist == 'Normal':
+                j = sns.jointplot(y=np.random.normal(np_well_mean, np_well_std, nsamples), x=np.random.normal(pi_mean, pi_std, nsamples), kind='hex', height=4)
+            elif metric_dist == 'Lognormal':
+                pass;
+            elif metric_dist == 'Triangular':
+                j = sns.jointplot(y=np.random.triangular(np_well[0],np_well_mean, np_well[1], nsamples), x=np.random.triangular(pi[0],pi_mean,pi[1], nsamples), kind='hex', height=4)
+            else:
+                j = sns.jointplot(y=np.random.uniform(np_well[0],np_well[1],nsamples), x=np.random.uniform(pi[0],pi[1],nsamples), kind='hex', height=4)
+            j.figure.axes[0].set_xlabel('PI')
+            j.figure.axes[0].set_ylabel('Np/well')
+            j.figure.axes[0].set_xlim((0, 200))
+            j.figure.axes[0].set_ylim((0, 100))
+            st.pyplot(j.figure, clear_figure=True)
 
 with tabFluid:
     col1, col2 = st.columns(2)
@@ -91,6 +120,7 @@ with tabFluid:
         st.pyplot(plotf.figure, clear_figure=True)
 
 with tabSampling:
+    st.subheader('MBAL Runs', divider='blue')
     nruns = st.number_input('MBAL runs:', 10, None, 100, step=100)
     pi_dist = np.random.normal(pi_mean, pi_std, nruns)
     np_well_dist = np.random.normal(np_well_mean, np_well_std, nruns)
@@ -115,9 +145,9 @@ with tabSampling:
     samples['Wells'] = samples['Np']/samples['Np_Well']
     samples = samples.round(0)
 
-    'Experiments'
+    st.subheader('Experiments', divider='blue')
     st.write(samples)
-    'Stats'
+    st.subheader('Stats', divider='blue')
     st.write(samples.describe().loc[['mean', '50%', 'std', 'min', 'max', 'count']].round(0))
 
 with tabSchedule:
@@ -125,33 +155,34 @@ with tabSchedule:
     interval = st.number_input('Interval between wells (days):', 0, None, None, step=30, help='teste help', placeholder='One new well every 90 days')
 
 with tabMBAL:
-    url = 'http://es00010252:2301/api/production/SatelliteOilLinearIPRWell/calculate'
+    pass;
+    # url = 'http://es00010252:2301/api/production/SatelliteOilLinearIPRWell/calculate'
 
-    for wct in ['0','10','20', '50', '95']:
-        st.write('WCT=',wct)
-        myobj = {
-            "fluid_gor": ["300"],
-            "fluid_api": ["22"],
-            "gas_sg": ["0.7"],
-            "gas_co2": ["0"],
-            "gas_h2s": ["0"],
-            "gas_n2": ["0"],
-            "fluid_wct": [wct],
-            "reservoir_pressure": ["350"],
-            "reservoir_temperature": ["55"],
-            "reservoir_depth": ["6000"],
-            "well_pi": ["12"],
-            "water_depth": ["1400"],
-            "riser_length": ["2000"],
-            "flowline_length": ["2000"],
-            "riser_diameter": ["6"],
-            "flowline_diameter": ["6"],
-            "pipe_type": ["FLX"],
-            "production_column_diameter": ["4.5"],
-            "arrival_pressure": ["90"]
-        }
-        x = requests.post(url, json = myobj)
-        st.write(x.text)
+    # for wct in ['0','10','20', '50', '95']:
+    #     st.write('WCT=',wct)
+    #     myobj = {
+    #         "fluid_gor": ["300"],
+    #         "fluid_api": ["22"],
+    #         "gas_sg": ["0.7"],
+    #         "gas_co2": ["0"],
+    #         "gas_h2s": ["0"],
+    #         "gas_n2": ["0"],
+    #         "fluid_wct": [wct],
+    #         "reservoir_pressure": ["350"],
+    #         "reservoir_temperature": ["55"],
+    #         "reservoir_depth": ["6000"],
+    #         "well_pi": ["12"],
+    #         "water_depth": ["1400"],
+    #         "riser_length": ["2000"],
+    #         "flowline_length": ["2000"],
+    #         "riser_diameter": ["6"],
+    #         "flowline_diameter": ["6"],
+    #         "pipe_type": ["FLX"],
+    #         "production_column_diameter": ["4.5"],
+    #         "arrival_pressure": ["90"]
+    #     }
+    #     x = requests.post(url, json = myobj)
+    #     st.write(x.text)
 
 
 with tabMatBal:
@@ -159,3 +190,5 @@ with tabMatBal:
 
 with tabResults:
     st.header("calma, estamos fazendo")
+
+st.caption('Powered by DND :sunglasses:')
